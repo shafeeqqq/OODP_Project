a869ab5c07ecec18750b543b6d3415aa481c07e0
MrESD <MrESD@LAPTOP-LQN5R0RM>
2018-10-24 13:56:05 +0800

    added some stuffs in course and faculty

--- Course.java
+++ Course.java
@@ -6,34 +6,58 @@ public class Course {
 	private int maxEnrollment;
 	
 
+	/**This method returns the courseCode as a String
+	 * @return courseCode
+	 */
 	public String getCourseCode() {
 		return courseCode;
 	}
 	
+	/**This method returns the courseCode as a String
+	 * @param code
+	 */
 	public void setCourseCode(String code) {
 		courseCode = code;
 	}
 	
+	/**
+	 * @return
+	 */
 	public String getcourseName() {
 		return courseName;
 	}
 	
+	/**
+	 * @param name
+	 */
 	public void setCourseName(String name) {
 		courseName = name;
 	}
 	
+	/**
+	 * @return
+	 */
 	public int getMaxEnrollment() {
 		return maxEnrollment;
 	}
 	
+	/**
+	 * @param max
+	 */
 	public void setMaxEnrollment(int max) {
 		maxEnrollment = max;
 	}
 	
+	/**
+	 * @return
+	 */
 	public FacultyStaff getCoordinator() {
 		return coordinator;
 	}
 	
+	/**
+	 * @param staff
+	 */
 	public void setCoordinator(FacultyStaff staff) {
 		coordinator = staff;
 	}
--- FacultyStaff.java
+++ FacultyStaff.java
@@ -1,9 +1,59 @@
 
 public class FacultyStaff {
 	
-	String staffID;
+	private String staffID;
+	private String staffName;
+	private boolean isCoordinator;
+	private boolean isProfessor;
+	private Course[] courseList;
+	private String facultyName;
+	
+	/**This method construct a FacultyStaff object
+	 * 
+	 */
 	public FacultyStaff() {
-		//constructor
+		
 	}
 	
+	/**
+	 * @return
+	 */
+	public String getStaffID(){
+		return staffID;
+	}
+	
+	/**
+	 * @return
+	 */
+	public String getStaffName() {
+		return staffName;
+	}
+	
+	/**
+	 * @return
+	 */
+	public boolean isCoordinator() {
+		return isCoordinator;
+	}
+	
+	/**
+	 * @return
+	 */
+	public boolean isProfessor() {
+		return isProfessor;
+	}
+	
+	/**
+	 * @return
+	 */
+	public Course[] getCourseList() {
+		return courseList;
+	}
+	
+	/**
+	 * @return
+	 */
+	public String getFacultyName() {
+		return facultyName;
+	}
 }
