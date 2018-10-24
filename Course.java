bdfaf5149e121446f32621219ca1fdf5ecefe72c
MrESD <MrESD@LAPTOP-LQN5R0RM>
2018-10-24 12:21:11 +0800

    removed falcuty typo

--- Course.java
+++ Course.java
@@ -1,4 +1,40 @@
 
 public class Course {
+	private String courseCode;
+	private String courseName;
+	private FacultyStaff coordinator;
+	private int maxEnrollment;
 	
+
+	public String getCourseCode() {
+		return courseCode;
+	}
+	
+	public void setCourseCode(String code) {
+		courseCode = code;
+	}
+	
+	public String getcourseName() {
+		return courseName;
+	}
+	
+	public void setCourseName(String name) {
+		courseName = name;
+	}
+	
+	public int getMaxEnrollment() {
+		return maxEnrollment;
+	}
+	
+	public void setMaxEnrollment(int max) {
+		maxEnrollment = max;
+	}
+	
+	public FacultyStaff getCoordinator() {
+		return coordinator;
+	}
+	
+	public void setCoordinator(FacultyStaff staff) {
+		coordinator = staff;
+	}
 }
