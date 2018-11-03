239623584895a4534b17b7fbe2feb19bbdac9af8
MrESD <MrESD@LAPTOP-LQN5R0RM>
2018-11-03 23:39:54 +0800

    a

--- Student.java
+++ Student.java
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public class Student {
-	private String studentName;
-	private String matricNo;
-	private HashMap<Semester, ArrayList<String>> candidature;
-	
-	public Student(String name, String matric) {
-		studentName = name;
-		matricNo = matric;		
-	}
-	public String getMatricNo() {
-		return matricNo;
-	}
-	
-	public String getStudentName() {
-		return studentName;	
-	}
-
-	public HashMap<Semester, ArrayList<String>> getCandidature() {
-		return candidature;
-	}
-	
-	public void printDetails() {
-		System.out.print(
-				"Name: " + studentName + "\n"
-			  + "Matriculation No.: " + matricNo + "\n");
-	}
-
-	
-	
-}
-=======
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -57,13 +22,14 @@ public class Student {
 	}
 	public void printCandidature() {
 		for (Semester sem : candidature.keySet()) {
-			System.out.println(sem.getYear() + " "+ );
-			System.out.println(candidature.get(sem));
-		}
+			System.out.println(sem.getYear() + " "+ sem.getNumber());
+			for (String course: candidature.get(sem)) {
+				System.out.print(course+ " ");
+			}
+			System.out.println();		}
 	}
 
 	public void setCandidature(Semester sem, ArrayList<String> stringArr) {
 		this.candidature.put(sem, stringArr);
 	}
 }
->>>>>>> branch 'master' of https://github.com/shafeeqqq/OODP_Project.git
