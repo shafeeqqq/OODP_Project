f50d3d828b8a931d71f2396a8f9ae21897c4b2cd
shafeeqqq <shafeeqqk@gmail.com>
2018-11-03 23:41:14 +0800

    a

--- Student.java
+++ Student.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Student {
+	
 	private String studentName;
 	private String matricNo;
 	private HashMap<Semester, ArrayList<String>> candidature;
@@ -10,16 +11,29 @@ public class Student {
 		studentName = name;
 		matricNo = matric;		
 	}
+	
+	
 	public String getMatricNo() {
 		return matricNo;
 	}
 	
+	
 	public String getStudentName() {
 		return studentName;	
 	}
+	
+	
 	public HashMap<Semester, ArrayList<String>> getCandidature() {
 		return candidature;
 	}
+	
+	public void printDetails() {
+		System.out.print(
+				"Name: " + studentName + "\n"
+			  + "Matriculation No.: " + matricNo + "\n");
+	}
+	
+	
 	public void printCandidature() {
 		for (Semester sem : candidature.keySet()) {
 			System.out.println(sem.getYear() + " "+ sem.getNumber());
