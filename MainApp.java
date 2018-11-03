568078d743ff8a1d02c5bdfbde761450f8ebeadf
shafeeqqq <shafeeqqk@gmail.com>
2018-11-03 15:43:17 +0800

    edited studentinfo

--- MainApp.java
+++ MainApp.java
@@ -1,11 +1,53 @@
+import java.util.Scanner;
 
 public class MainApp {
 
 	public static University university;
+	static Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
+		university = new University("NTU");
 		
+		printMainMenu();
+		int choice = getChoice();
+		
+		switch(choice) {
+		case 1:
+			launchInterface("admin");
+			break;
+			
+		case 2:
+			launchInterface("student");
+		}
+	}
 
+	private static void launchInterface(String type) {
+		if (type.equals("admin")) {
+			String matricNo = getMatricNo();
+			
+			
+		}
+		
+	}
+
+	private static String getMatricNo() {
+		System.out.println("Enter matric No: ");
+		
+		String matricNo = sc.nextLine();
+		while (university.)
+		return null;
+	}
+
+	private static int getChoice() {
+		int choice = sc.nextInt();
+		return choice;
+	}
+
+	private static void printMainMenu() {
+		System.out.print(
+				  "1) admin"
+				+ "2) student"); 
+		
 	}
 
 }
--- Semester.java
+++ Semester.java
@@ -2,4 +2,30 @@
 public class Semester {
 	private int year;
 	private int number;
+	
+	Semester(int year, int number) {
+		this.year = year;
+		this.number = number;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+	
+	public String toString() {
+		return (year + " Semester " + number);
+	}
+	
+	public boolean equals(Semester semester) {
+		if (semester.getYear() == this.year && semester.getNumber() == this.number)
+			return true;
+		else
+			return false;
+	}
+	
+	
 }
