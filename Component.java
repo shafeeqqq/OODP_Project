39f99586a8189a258fa64d7421518ebae59caf9c
MrESD <MrESD@LAPTOP-LQN5R0RM>
2018-11-03 15:41:05 +0800

    sounds dogshittttt

--- Component.java
+++ Component.java
@@ -1,6 +1,20 @@
-
 public class Component {
 	private String title;
 	private int weightage;
-	
+	public Component(String componentTitle,int componentWeightage) {
+		title=componentTitle;
+		weightage=componentWeightage;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String componentTitle) {
+		title=componentTitle;
+	}
+	public int getWeightage() {
+		return weightage;
+	}
+	public void setWeightage(int componentWeightage) {
+		weightage=componentWeightage;
+	}
 }
