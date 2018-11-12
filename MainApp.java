81f51834c92125edb00e31100f60742ae16b8beb
MrESD <MrESD@LAPTOP-LQN5R0RM>
2018-11-12 10:07:38 +0800

    added javadoc for main

--- MainApp.java
+++ MainApp.java
@@ -2,10 +2,20 @@ import java.io.IOException;
 import java.util.Scanner;
 //display all listing of students after addition of 1 student
 public class MainApp {
+	/**
+	 * the main interfaces through university to other classes
+	 */
 	public static University university;
+	/**
+	 * this object reads and saves to the respective files
+	 */
 	public static FileIO file;
 	static Scanner sc = new Scanner(System.in);
 	
+	/**
+	 * The main function gives users 3 choices,
+	 * to use as admin, students or to save the changes
+	 */
 	public static void main(String[] args) {
 		university = new University("NTU");
 		file = new FileIO(university);
@@ -46,13 +56,19 @@ public class MainApp {
 			choice = getChoice();			
 	}
 }
-
+	/**
+	 * this method saves all the data to the respective files
+	 * @throws IOException
+	 */
 	private static void Saves() throws IOException {
 		file.saveStudents("students.txt",university.getAllStudents());
 		file.saveStaffs("facultystaff.txt", university.getAllStaffs());
 		file.saveCourses("courses.txt",university.getallFaculty());
 	}
-
+	/**
+	 * this method launches 2 interface, admin if mode is 1 and student if mode is 2
+	 * @param mode
+	 */
 	private static void launchInterface(String mode) {
 		if (mode.equals("student")) {
 			String matricNo = getMatricNo();
@@ -74,14 +90,21 @@ public class MainApp {
 		
 	}
 
-	
+	/**
+	 * this method gets the input of the user to be used as matricNo to be used to instantiate
+	 * student object
+	 * @return matricNo
+	 */
 	private static String getMatricNo() {
 		System.out.println("Enter matric No: ");
 		String matricNo = sc.nextLine();
 		return matricNo;
 	}
 
-	
+	/**
+	 * this method gets an integer input from user
+	 * @return a integer
+	 */
 	private static int getChoice() {
 		System.out.println("Enter choice: ");
 		boolean error = true;
@@ -102,7 +125,9 @@ public class MainApp {
 		return choice;
 	}
 
-	
+	/**
+	 * this method prints the main menu inside the main function
+	 */
 	private static void printMainMenu() {
 		System.out.print(
 				  "### Main Menu ###\n"
